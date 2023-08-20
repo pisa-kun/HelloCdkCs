6a4c3954dc19092f7b8d742a5baf9fbbcecc0e77
pisa-kun <pisa0401mst@gmail.com>
2023-08-20 22:55:39 +0900

    first commit

---
+++ src/HelloCdkCs/HelloCdkCsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace HelloCdkCs
+{
+    public class HelloCdkCsStack : Stack
+    {
+        internal HelloCdkCsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
